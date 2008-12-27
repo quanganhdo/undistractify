44b937ae7897b4e29a80c46b3ee0f700042e2d17
Quang Anh Do <quanganhdo@gmail.com>
2008-12-27 16:42:16 +0000

    Add initial SQL dump

---
+++ app/config/sql/undistraction_dump.sql
@@ -0,0 +1,36 @@
+# Sequel Pro dump
+# Version 254
+# http://code.google.com/p/sequel-pro
+#
+# Host: localhost (MySQL 5.0.41)
+# Database: undistraction
+# Generation Time: 2008-12-27 16:38:05 +0000
+# ************************************************************
+
+# Dump of table logs
+# ------------------------------------------------------------
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL auto_increment,
+  `date` date default NULL,
+  `url_id` char(36) default NULL,
+  `visits` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table urls
+# ------------------------------------------------------------
+
+CREATE TABLE `urls` (
+  `id` char(36) NOT NULL default '',
+  `name` varchar(255) default NULL,
+  `address` text,
+  `limit` int(11) default NULL,
+  `created` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
