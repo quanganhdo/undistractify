919cfb4b114f29a2d707981c344e45ff749ad27b
Quang Anh Do <quanganhdo@gmail.com>
2008-12-27 19:30:43 +0000

    Update database. Remove logs (for now)

--- app/config/sql/undistraction_dump.sql
+++ app/config/sql/undistraction_dump.sql
@@ -7,27 +7,14 @@
 # Generation Time: 2008-12-27 16:38:05 +0000
 # ************************************************************
 
-# Dump of table logs
-# ------------------------------------------------------------
-
-CREATE TABLE `logs` (
-  `id` int(11) NOT NULL auto_increment,
-  `date` date default NULL,
-  `url_id` char(36) default NULL,
-  `visits` int(11) default NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
-
 # Dump of table urls
 # ------------------------------------------------------------
 
 CREATE TABLE `urls` (
   `id` char(36) NOT NULL default '',
   `name` varchar(255) default NULL,
-  `address` text,
-  `limit` int(11) default '10',
+  `address` text, 	
+  `lastvisit` datetime default NULL,
   `created` datetime default NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
