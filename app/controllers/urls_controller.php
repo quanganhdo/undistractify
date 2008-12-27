d6671815e614b803c3da348a782094353d8d577a
Quang Anh Do <quanganhdo@gmail.com>
2008-12-27 20:45:46 +0000

    Add basic redirection

--- app/controllers/urls_controller.php
+++ app/controllers/urls_controller.php
@@ -4,7 +4,17 @@ class UrlsController extends AppController {
 	var $name = 'Urls';
 	
 	function index() {
+		$this->pageTitle = __('Home', true);
 		$this->set('urls', $this->Url->findAll());
+	}
+	
+	function view($id = null) {
+		if (!$id) {
+			$this->redirect('/');
+		}
+		
+		$url = $this->Url->findById($id);
+		$this->redirect($url['Url']['address']);
 	}
 	
 }
--- app/views/urls/index.ctp
+++ app/views/urls/index.ctp
@@ -1,7 +1,7 @@
 <ul>
 <?php foreach ((array) $urls as $url): ?>
 	<li>
-	<?php echo $html->link($url['Url']['name'], $url['Url']['address']) ?>	
+		<?php echo $html->link($url['Url']['name'], array('controller' => 'urls', 'action' => 'view', 'id' => $url['Url']['id'])) ?>	
 	</li>
 <?php endforeach ?>
 </ul>
