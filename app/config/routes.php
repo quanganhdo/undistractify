32134490dcd11bc58fa7348eb81088fdfc54abe1
Quang Anh Do <quanganhdo@gmail.com>
2009-01-09 18:12:06 +0000

    Update route configuration

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,5 +1,6 @@
 <?php
-	Router::connect('/', array('controller' => 'urls', 'action' => 'index'));
+	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+	Router::connect('/:name', array('controller' => 'users', 'action' => 'view'), array('pass' => array('name')));
 	Router::connect('/go/:id', array('controller' => 'urls', 'action' => 'view'), array('pass' => array('id')));
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 ?>
