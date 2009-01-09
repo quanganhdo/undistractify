ea3f747f3faf581132e132671150aaacb8de683f
Quang Anh Do <quanganhdo@gmail.com>
2009-01-09 17:04:04 +0000

    Change URL from /:id to /go/:id

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,5 +1,5 @@
 <?php
 	Router::connect('/', array('controller' => 'urls', 'action' => 'index'));
-	Router::connect('/:id', array('controller' => 'urls', 'action' => 'view'), array('pass' => array('id')));
+	Router::connect('/go/:id', array('controller' => 'urls', 'action' => 'view'), array('pass' => array('id')));
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
 ?>
